be9eb0a730f8463d005bcd8fbe823a9ad3847a6d
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2025-01-29 22:38:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
   challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    tully: "Cr3scent",
-    hudson: "Cr3scent",
-    ella: "dexter"
+    Tully: "Cr3scent",
+    Hudson: "Cr3scent",
+    Ella: "dexter"
   },
 };
 
